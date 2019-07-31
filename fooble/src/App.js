8f4a5d081da16e9153507a376aa4dcacd563dccf
Syed-Rafi-Naqvi <sd.rafi1819@gmail.com>
2019-07-31 20:04:50 +0530

    navbar and body added to landing page

---
+++ fooble/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import './App.css';
+
+import Navbar from './landingPageComponent/navbar/navbar';
+import Body from './landingPageComponent/body/body';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar />
+        <Body />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ fooble/src/landingPageComponent/body/body.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './body.css'
+
+class body extends Component {
+    render() {
+        return (
+            <div className = 'body'>
+                <h1>TheFooBle.in</h1>
+            </div>
+        )
+    }
+}
+
+export default body
